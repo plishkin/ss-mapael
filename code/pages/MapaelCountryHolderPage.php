0ef5e184a94b1ce9f39b44c58d0dc81974c9e63b
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:09:51 +1300

    Converted to PSR-2

--- code/pages/MapaelCountryHolderPage.php
+++ code/pages/MapaelCountryHolderPage.php
@@ -2,15 +2,14 @@
 
 
 
-class MapaelCountryHolderPage extends Page {
+class MapaelCountryHolderPage extends Page
+{
 
     private static $singular_name = 'Country Holder Page';
 
     private static $plural_name = 'Country Holder Pages';
-
-
 }
 
-class MapaelCountryHolderPage_Controller extends Page_Controller {
-
+class MapaelCountryHolderPage_Controller extends Page_Controller
+{
 }
